8e7b4fa0127b8d94caa25695561f438788775a0b
Martin Mwenda <martinmwenda026@gmial.com>
2021-11-26 17:17:08 +0300

    album

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,12 +1,8 @@
 import { Track } from "./track";
 
 export interface Album {
-  id: number;
-  artist: string;
-  album: {
     name: string;
     releaseDate: string;
     coverImage: string;
     tracks: Track[];
-  };
 }
